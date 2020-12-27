1b366d271db3aae2f087580b9b9a9fea16c9f54a
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-12-27 17:59:47 +0530

    server functional, react app created

---
+++ app/backend/server.js
@@ -0,0 +1,29 @@
+// Imports
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Get environment
+require('dotenv').config();
+
+// Create express web app, specify port
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Necessary specifications for functioning
+app.use(cors());
+app.use(express.json());
+
+// Connect to DB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+// Start app
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
